fd48597ce3ce4bb8d4c7777bfa6d159b899e234b
Javin Mehta <javin.mehta@magnetforensics.com>
2023-10-04 11:18:43 -0400

    ability to query google books api

--- Backend/src/__init__.py
+++ Backend/src/__init__.py
@@ -1,10 +1,37 @@
 from flask import Flask, Response, request
+import requests
+from src.google import SearchGoogle
+import json
 
-def create_app(test_config=None):
+def exception_handler(message):
+    status_code = 400
+    return {"status_code": status_code, "message": message}
+    return Response(str(message), status_code)
+
+def create_app():
     app = Flask(__name__)
+    search = SearchGoogle()
 
     @app.route("/hello")
     def start():
         return "Hello World!"
     
+    @app.route("/getByIsbn", methods=['POST'])
+    def searchByIsbn():
+        payload = request.get_json()
+        attrs = ['keywords', 'title', 'author', 'publisher', 'subject', 'isbn', 'lccn', 'oclc']
+        queryDict = {}
+        for attr in attrs:
+            queryDict[attr] = payload.get(attr, None)
+        if len(attrs) == sum(1 for v in queryDict.values() if v is None):
+            return exception_handler("Querying a book requires a field")
+        try:
+            result = search.searchGoogleApi(queryDict)
+            print(result)
+            return json.dumps(result)
+        except Exception as e:
+            return exception_handler("Failed to query book, " + e.message())
+
+    # http://127.0.0.1:5000/getByIsbn/9781787285613
+
     return app
---
+++ Backend/src/google.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+class SearchGoogle:
+    def __init__(self):
+        pass
+
+    def searchGoogleApi(self, queryParams):
+        query = ""
+        for key, value in queryParams.items():
+            if key == "keywords" and value is not None:
+                words = value.split(' ')
+                for word in words:
+                    query += "{}+".format(word)
+            elif value is not None:
+                query += "{}:{}+".format(key, value)
+        query = query[:-1]
+        output = requests.get('https://www.googleapis.com/books/v1/volumes?q={}&filter=ebooks'.format(query))
+        return ({
+            'query': query,
+            'results': self.parseGoogleResults(output.content)
+            })
+
+    def parseGoogleResults(self, content):
+        content = json.loads(content)
+        totalResults = content.get("totalItems", 0)
+        bookInfo = []
+        for item in content.get("items", []):
+             itemDetails = {}
+             itemDetails["selfLink"] = item.get("selfLink", "Unknown")
+             itemDetails["volumeInfo"] = item.get("volumeInfo", "Unknown")
+             itemDetails['saleInfo'] = item.get("saleInfo", "Unknown")
+             bookInfo.append(itemDetails)
+        return bookInfo
